0916395be8cab74574038fe1371811615f1b6c9c
drfu <fdr0801@163.com>
2016-05-17 16:31:14 +0800

    Update jquery.message.js

--- src/js/modules/jquery.message.js
+++ src/js/modules/jquery.message.js
@@ -2,71 +2,6 @@
  * Created by drfu on 2016/5/11.
  */
 ;(function(jQuery) {
-
-    /**
-     * 定义正则表达式
-     * @type {{color: RegExp, number: RegExp, int: RegExp, account: RegExp}}
-     * @private
-     */
-    var regexps = {
-        "color": /^(#[a-fA-F0-9]{3})|(#[a-fA-F0-9]{6})$/ig,
-        "number": /^\-?\d+(\.\d+)?$/ig,
-        "positiveNumber": /^\d+(\.\d+)?$/ig,
-        "int": /^\-?\d+$/ig,
-        "positiveInteger": /^\d+$/ig,
-        "account": /^[a-zA-Z_]\w{2,14}[a-zA-Z0-9_]$/ig,
-        "ip": /^((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d)(.((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d)){3}$/ig,
-        "host": /^[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+.?$/ig,
-        "url": new RegExp('^((https|http|ftp|rtsp|mms)?://)'
-            + '(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' //ftp的user@
-            + '(([0-9]{1,3}.){3}[0-9]{1,3}' // IP形式的URL- 199.194.52.184
-            + '|' // 允许IP和DOMAIN（域名）
-            + '([0-9a-z_!~*\'()-]+.)*' // 域名- www.
-            + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名
-            + '[a-z]{2,6})' // first level domain- .com or .museum
-            + '(:[0-9]{1,4})?' // 端口- :80
-            + '((/?)|' // a slash isn't required if there is no file name
-            + '(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$', "ig"),
-        "mail": /^[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?$/ig,
-        "tel": /(^\d{11}$)|(^((\+?86)|(\(\+86\)))?(\d{3,4})?(\-|\s)?\d{7,8}((\-|\s)\d{3,4})?$)/ig,
-        "pass": /^\S{6,16}$/ig,
-        "pxsize": /^\d+px$/ig
-    };
-
-
-    var regex = {
-        "check": function() {
-
-        }
-    };
-
-    /**
-     * 生成UUID
-     * @type {{get: uuid.get, short: uuid.short}}
-     */
-    window.uuid = {
-        /**
-         * 创建UUID
-         * @returns {string}
-         */
-        create: function () {
-            var d = new Date().getTime();
-            var _uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = (d + Math.random() * 16) % 16 | 0;
-                d = Math.floor(d / 16);
-                return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
-            });
-            return _uuid;
-        },
-        /**
-         * 创建一个短格式的UUID
-         * @returns {XML|void|string}
-         */
-        simple: function (header) {
-            return (header || "") + uuid.get().replace(/\-/ig, "");
-        }
-    };
-
     /**
      * 格式化字符串
      * @param args
@@ -198,11 +133,13 @@
     var confirm = {
 
         events: function(submit, cancel) {
-            jQuery("div.msg-message").on("click", "> .msg-button .submit", submit).on("click", "> .msg-button .cancel", cancel);
+            jQuery("div.msg-message").on("click", "> .msg-button .submit", function () {
+                $(".msg-bg").fadeOut("fast");
+                submit();
+            }).on("click", "> .msg-button .cancel", cancel);
         },
 
-        init: function(setting, submit, cancel) {
-
+        init: function(type, setting, submit, cancel) {
             var defStyle = {
                     bg: {
                         backgroundColor: "#eaeaea",
@@ -220,80 +157,137 @@
                         left: "50%",
                         top: "50%"
                     },
-                    title: {
-                        padding: "10px 10px",
-                        backgroundColor: "#39aeff",
-                        fontSize: "15px",
-                        color: "#fff"
-                    },
                     content: {
                         padding: "10px 10px"
-                    },
-                    button: {
-                        width: "50%",
-                        fontSize: "13px",
-                        padding: "10px 0",
-                        textAlign: "center",
-                        border: "none",
-                        fontFamily: "Microsoft YaHei UI",
-                        color: "#fff",
-                        cursor: "pointer",
-                        outline: "none"
-                    },
-                    buttonSubmit: {
-                        backgroundColor: "#3296f4"
-                    },
-                    buttonCancel: {
-                        backgroundColor: "#838383"
                     }
                 },
-                defSetting = {
-                title: "title",
-                ico: "",
-                content: "content",
-                submit: "ok",
-                cancel: "no",
-                style: defStyle
-            };
+                defSetting = {};
+
+            switch (type) {
+                case "confirm":
+                    $.extend(true, defStyle, {
+                        title: {
+                            padding: "10px 10px",
+                            backgroundColor: "#39aeff",
+                            fontSize: "15px",
+                            color: "#fff"
+                        },
+                        button: {
+                            width: "50%",
+                            fontSize: "13px",
+                            padding: "10px 0",
+                            textAlign: "center",
+                            border: "none",
+                            fontFamily: "Microsoft YaHei UI",
+                            color: "#fff",
+                            cursor: "pointer",
+                            outline: "none"
+                        },
+                        buttonSubmit: {
+                            backgroundColor: "#3296f4"
+                        },
+                        buttonCancel: {
+                            backgroundColor: "#838383"
+                        }
+                    });
+                    defSetting = {
+                        title: "title",
+                        ico: "",
+                        content: "content",
+                        submit: "ok",
+                        cancel: "no",
+                        style: defStyle
+                    };
+                    break;
+                case "message":
+                    $.extend(true, defStyle, {
+                        button: {
+                            padding: "8px",
+                            fontSize: "13px",
+                            textAlign: "center",
+                            border: "none",
+                            fontFamily: "Microsoft YaHei UI",
+                            cursor: "pointer",
+                            outline: "none",
+                            position: "absolute",
+                            right: "10px"
+                        },
+                        buttonSubmit: {
+                            backgroundColor: "transparent",
+                            color: "#000"
+                        }
+                    });
+                    defSetting = {
+                        content: "show your message",
+                        submit: "ok",
+                        style: defStyle
+                    };
+                    break;
+                case "shortmessage":
+
+                    break;
+            }
             if($.isPlainObject(setting) && !$.isEmptyObject(setting)) {
                 $.extend(true, defSetting, setting);
             }
-            var bg = '<div class="msg-bg"></div>',
+            var bg = '<div class="msg-bg"></div>', msgHtml = "";
+            if(type === "confirm"){
                 msgHtml = '<div class="msg-message"> <div class="msg-title"> <span class="{0}"></span> <span>{1}</span> </div> <div class="msg-content"> {2} </div> <div class="msg-button"> <button class="submit">{3}</button><button class="cancel">{4}</button> </div> </div>'.format(defSetting.ico, defSetting.title, defSetting.content, defSetting.submit, defSetting.cancel);
+            }else if(type === "message") {
+                msgHtml = '<div class="msg-message"> <div class="msg-content"> {0} </div> <div class="msg-button"> <button class="submit">{1}</button></div> </div>'.format(defSetting.content, defSetting.submit);
+            }
 
             _body.prepend($(bg).append(msgHtml));
 
             var ms = $(".msg-message");
             $(".msg-bg").css(defSetting.style.bg);
             ms.css(defSetting.style.wrap);
-            $(".msg-title").css(defSetting.style.title);
             $(".msg-content").css(defSetting.style.content);
-            $(".msg-button > button").css(defSetting.style.button);
             $(".msg-button > button.submit").css(defSetting.style.buttonSubmit);
-            $(".msg-button > button.cancel").css(defSetting.style.buttonCancel);
+            if(type === "confirm") {
+                $(".msg-title").css(defSetting.style.title);
+                $(".msg-button > button").css(defSetting.style.button);
+                $(".msg-button > button.cancel").css(defSetting.style.buttonCancel);
+            }else if(type === "message") {
+                $(".msg-button").css({position: "relative"});
+                $(".msg-button > button").css(defSetting.style.button);
+            }
+
             var w = ms.width(),
                 h = ms.height();
             ms.css({
                 marginLeft: -(w/2) + "px",
                 marginTop: -(h/2) + "px"
             });
-            if($.isFunction(submit) || $.isFunction(cancel))
-                confirm.events(submit, cancel);
+            if($.isFunction(submit) || $.isFunction(cancel)) {
+                if(type === "confirm") {
+                    confirm.events(submit, cancel);
+                }else if(type === "message") {
+                    confirm.events(submit);
+                }
+            }
+
         }
     };
 
     jQuery.extend({
-        message: function(type, setting, submit, cancel) {
-            switch (type) {
-                case "tooltip":
-                    tooltip.init(setting);
-                    break;
-                case "confirm":
-                case "message":
-                    confirm.init(setting, submit, cancel);
-                    break;
-            }
+        tooltip: function(setting) {
+            tooltip.init(setting);
         }
     });
-    jQuery.fn.regex = regex.check();
+    jQuery.extend({
+        confirm: function(setting, submit, cancel) {
+            confirm.init("confirm", setting, submit, cancel);
+        }
+    });
+    jQuery.extend({
+        message: function(setting, submit) {
+            confirm.init("message", setting, submit);
+        }
+    });
+    jQuery.extend({
+        shortmessage: function(setting, submit, cancel) {
+            confirm.init("shortmessage", setting, submit, cancel);
+        }
+    });
 })(jQuery);
